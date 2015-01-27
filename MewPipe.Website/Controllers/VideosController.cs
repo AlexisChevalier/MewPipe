6dfd24c38f5344ae3ea2ffbed2059a2684de863d
Alexis Chevalier <alexis.chevalier1@gmail.com>
2015-01-27 09:11:00 +0100

    Added missing files

---
+++ MewPipe.Website/Controllers/VideosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MewPipe.Website.Controllers
+{
+    public class VideosController : Controller
+    {
+        /**
+         * Video player page
+         */
+        [Route("v/{videoId}", Name = "ViewVideo")]
+        public ActionResult Index(string videoId)
+        {
+            return View();
+        }
+    }
+}
