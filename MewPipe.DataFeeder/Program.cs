fbdd1e0c6ffb7d644a2bf012511f022d1c980dd7
Citizen01 <jeanbaptiste.e@hotmail.fr>
2015-06-04 20:12:05 +0200

    Added YoutubeExctractor + Forgot .sln

--- MewPipe.DataFeeder/Program.cs
+++ MewPipe.DataFeeder/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace MewPipe.DataFeeder
+﻿using System;
+
+namespace MewPipe.DataFeeder
 {
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				ShowUsage();
+			}
+		}
+
+		private static void ShowUsage()
+		{
+			Console.WriteLine("USAGE: DataFeeder.exe <pathToCsvFile>");
 		}
 	}
 }
