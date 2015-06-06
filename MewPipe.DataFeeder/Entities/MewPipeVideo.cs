97bb776365b5d3f224d502b83cdf3a48c6f5a84c
Citizen01 <jeanbaptiste.e@hotmail.fr>
2015-06-07 00:45:59 +0200

    Now getting extra videos datas from yt (description, views, category)

---
+++ MewPipe.DataFeeder/Entities/MewPipeVideo.cs
@@ -0,0 +1,11 @@
+﻿namespace MewPipe.DataFeeder.Entities
+{
+	public class MewPipeVideo
+	{
+		public string FilePath { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public long Views { get; set; }
+		public string Category { get; set; }
+	}
+}
--- MewPipe.DataFeeder/Utils/VideoManager.cs
+++ MewPipe.DataFeeder/Utils/VideoManager.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using MewPipe.DataFeeder.Entities;
 using YoutubeExtractor;
 
 namespace MewPipe.DataFeeder.Utils
@@ -9,6 +14,46 @@ namespace MewPipe.DataFeeder.Utils
 	public static class VideoManager
 	{
 		private static readonly string _cacheFolder = @"_cache";
+
+		public static MewPipeVideo Download(string url)
+		{
+			IEnumerable<VideoInfo> vInfos = DownloadUrlResolver.GetDownloadUrls(url);
+			var videoInfos = vInfos.Where(v => v.VideoType == VideoType.Mp4).ToList();
+			var lowestResolution = GetLowestResolution(videoInfos);
+			VideoInfo video = videoInfos.First(v => v.Resolution == lowestResolution);
+
+			// Making sure the cache folder does exist
+			CreateCacheFolder();
+
+			var videoTitle = GetSafeVideoTitle(video.Title);
+			var filePath = Path.Combine(_cacheFolder, videoTitle + ".mp4");
+
+			var mewPipeVideo = ConvertToMewPipeVideo(video, url);
+			mewPipeVideo.FilePath = filePath;
+
+			// Checking if we have that video in the cache folder
+			if (IsVideoAlreadyCached(videoTitle))
+			{
+				Console.WriteLine("Video " + videoTitle + " is already cached ! Skipping download ...");
+				return mewPipeVideo;
+			}
+
+			// We don't have that video in the cache folder, Downloading it ...
+
+			var videoDownloader = new VideoDownloader(video, filePath);
+
+			// Register the ProgressChanged event and print the current progress
+			videoDownloader.DownloadProgressChanged +=
+				(sender, args) => Console.Write("\rDownloading: " + Math.Floor(args.ProgressPercentage) + "%");
+
+			Console.WriteLine("Downloading " + videoTitle + " ...");
+			videoDownloader.Execute();
+			Console.WriteLine("\nDownloaded " + videoTitle + " !\n");
+
+			return mewPipeVideo;
+		}
+
+		#region Private Helpers
 
 		private static int GetLowestResolution(IEnumerable<VideoInfo> vInfos)
 		{
@@ -37,36 +82,62 @@ namespace MewPipe.DataFeeder.Utils
 			return videoTitle.Replace("/", "").Replace("|", "").Replace(":", "");
 		}
 
-		public static string Download(string url)
+		private static MewPipeVideo ConvertToMewPipeVideo(VideoInfo vInfos, string url)
 		{
-			IEnumerable<VideoInfo> vInfos = DownloadUrlResolver.GetDownloadUrls(url);
-			var videoInfos = vInfos.Where(v => v.VideoType == VideoType.Mp4).ToList();
-			var lowestResolution = GetLowestResolution(videoInfos);
-			VideoInfo video = videoInfos.First(v => v.Resolution == lowestResolution);
+			string html = GetWebPage(url);
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml(html);
 
-			// Making sure the cache folder does exist
-			CreateCacheFolder();
+			var viewsStr = doc.GetElementbyId("watch7-views-info").Element("div").InnerText;
+			viewsStr = Regex.Replace(viewsStr, "[^.0-9]", ""); // Removes all non-numeric characters
+			viewsStr = Regex.Replace(viewsStr, @"\u00A0", ""); // Removes all non-breakable spaces
+			var views = long.Parse(viewsStr);
 
-			var videoTitle = GetSafeVideoTitle(video.Title);
-			var filePath = Path.Combine(_cacheFolder, videoTitle + ".mp4");
+			var description = doc.GetElementbyId("eow-description").InnerText;
 
-			if (IsVideoAlreadyCached(videoTitle))
+			var category = doc
+				.GetElementbyId("watch-description-extras")
+				.Element("ul")
+				.Element("li")
+				.Element("ul")
+				.Element("li")
+				.InnerText;
+
+			return new MewPipeVideo
 			{
-				Console.WriteLine("Video " + videoTitle + " is already cached ! Skipping download ...");
-				return filePath;
-			}
-
-			var videoDownloader = new VideoDownloader(video, filePath);
-
-			// Register the ProgressChanged event and print the current progress
-			videoDownloader.DownloadProgressChanged +=
-				(sender, args) => Console.Write("\rDownloading: " + Math.Floor(args.ProgressPercentage) + "%");
-
-			Console.WriteLine("Downloading " + videoTitle + " ...");
-			videoDownloader.Execute();
-			Console.WriteLine("\nDownloaded " + videoTitle + " !\n");
-
-			return filePath;
+				Title = vInfos.Title,
+				Description = description,
+				Views = views
+			};
 		}
+
+		private static string GetWebPage(string url)
+		{
+			string result = null;
+			WebResponse response = null;
+			StreamReader reader = null;
+
+			try
+			{
+				HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+				request.Method = "GET";
+				response = request.GetResponse();
+				reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+				result = reader.ReadToEnd();
+			}
+			catch (Exception ex)
+			{
+			}
+			finally
+			{
+				if (reader != null)
+					reader.Close();
+				if (response != null)
+					response.Close();
+			}
+			return result;
+		}
+
+		#endregion
 	}
 }
