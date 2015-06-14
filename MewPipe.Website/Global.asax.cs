b0df32789c4b10dbe25329cf495de2809a566330
Citizen01 <jeanbaptiste.e@hotmail.fr>
2015-06-15 01:09:13 +0200

    Changed error message when session expired (inactivity or account deleted)

--- MewPipe.Website/Global.asax.cs
+++ MewPipe.Website/Global.asax.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,28 +9,30 @@ using MewPipe.Website.Security;
 
 namespace MewPipe.Website
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+	public class MvcApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
 
-        void Application_Error(object sender, EventArgs e)
-        {
-            var exc = Server.GetLastError();
-            
-            if (exc is OauthExpiredTokenException)
-            {
-                HttpContext.Current.SaveIdentityFromContext(new Identity());
-                FlashMessageExtensions.CreateCookieWithFlashMessage(FlashMessageExtensions.Notification.Warning, "You have been disconnected because of a long inactivity period !");
-                Server.ClearError();
-                Response.Redirect("/", false);
-                Context.ApplicationInstance.CompleteRequest();
-            }
-        }
-    }
+		private void Application_Error(object sender, EventArgs e)
+		{
+			var exc = Server.GetLastError();
+
+			if (exc is OauthExpiredTokenException)
+			{
+				HttpContext.Current.SaveIdentityFromContext(new Identity());
+				FlashMessageExtensions.CreateCookieWithFlashMessage(
+					FlashMessageExtensions.Notification.Warning,
+					"You have been disconnected.");
+				Server.ClearError();
+				Response.Redirect("/", false);
+				Context.ApplicationInstance.CompleteRequest();
+			}
+		}
+	}
 }
